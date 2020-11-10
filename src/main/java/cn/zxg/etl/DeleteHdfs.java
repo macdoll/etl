e2b3c8edd7f102031bb2cb595a24f6b4638130e0
朱兴国 <2446256163@qq.com>
2020-11-10 13:40:32 +0800

    有土豆粉

---
+++ src/main/java/cn/zxg/etl/DeleteHdfs.java
@@ -0,0 +1,18 @@
+package cn.zxg.etl;
+
+/**
+ * @param
+ * @author zhu_xingguo
+ * @description
+ * @time 2020/11/10 11:34
+ * @return
+ */
+
+/**
+ * 查询指定路径下的文件大小
+ * 如果文件大小为20b
+ * 删除该数据块
+ */
+public class DeleteHdfs {
+
+}
---
+++ src/main/java/cn/zxg/etl/TestHdfsClinet.java
@@ -0,0 +1,22 @@
+package cn.zxg.etl;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * @param
+ * @author zhu_xingguo
+ * @description
+ * @time 2020/11/10 11:44
+ * @return
+ */
+
+/**
+ * psvm输出快捷方式
+ */
+public class TestHdfsClinet {
+    public static void main(String[] args) {
+
+    }
+}
